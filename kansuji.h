4d3798ddd4cb588cc8e91c52dfad38b1891e2655
algon-320 <algon.0320@gmail.com>
2020-03-24 18:25:09 +0900

    マクロを整形

--- kansuji.h
+++ kansuji.h
@@ -1,21 +1,21 @@
 #pragma once
 
-#define 一 *0 + 1ll + 0
-#define 二 *0 + 2ll + 0
-#define 三 *0 + 3ll + 0
-#define 四 *0 + 4ll + 0
-#define 五 *0 + 5ll + 0
-#define 六 *0 + 6ll + 0
-#define 七 *0 + 7ll + 0
-#define 八 *0 + 8ll + 0
-#define 九 *0 + 9ll + 0
-#define 十 )*10ll + (1ll
-#define 百 )*100ll + (1ll
-#define 千 )*1000ll + (1ll
-#define 万 *0))*10000ll+((1ll
-#define 億 *0))*100000000ll+((1ll
-#define 兆 *0))*1000000000000ll+((1ll
-#define 京 *0))*10000000000000000ll+((1ll
+#define 一 * 0 + 1ll + 0
+#define 二 * 0 + 2ll + 0
+#define 三 * 0 + 3ll + 0
+#define 四 * 0 + 4ll + 0
+#define 五 * 0 + 5ll + 0
+#define 六 * 0 + 6ll + 0
+#define 七 * 0 + 7ll + 0
+#define 八 * 0 + 8ll + 0
+#define 九 * 0 + 9ll + 0
+#define 十 ) * 10ll + (1ll
+#define 百 ) * 100ll + (1ll
+#define 千 ) * 1000ll + (1ll
+#define 万 * 0)) * 10000ll + ((1ll
+#define 億 * 0)) * 100000000ll + ((1ll
+#define 兆 * 0)) * 1000000000000ll + ((1ll
+#define 京 * 0)) * 10000000000000000ll + ((1ll
 #define KANSUJI(ex) ((1ll ex * 0))
 
 void kansuji_print(long long x)
